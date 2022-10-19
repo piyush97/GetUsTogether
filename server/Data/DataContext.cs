1c9791e8e37f1ee3afacb721b90e2e470903b424
piyush97 <me@piyushmehta.com>
2022-10-18 23:46:34 -0400

    connectionstring and setup of dependency injection

--- server/Data/DataContext.cs
+++ server/Data/DataContext.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using server.Entities;
 
 namespace Server.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
+        public DbSet<AppUser> Users { get; set; }
     }
 
 }
